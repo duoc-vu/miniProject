f4aa5af7b2c7286ae4b14c8832d5e4ad528d7e31
duocvu <21A100100100@students.hou.edu.vn>
2024-09-12 09:41:25 +0700

    CRUD post

--- app.js
+++ app.js
@@ -45,10 +45,7 @@ app.get("/login", (req ,res ) =>{
     res.render("login")
 })
 
-app.get("/profile", isLoggedIn, (req ,res ) =>{
-    console.log(req.user);
-    res.render("login")
-})
+
 
 
 
@@ -63,13 +60,60 @@ app.post("/login", async (req,res) =>{
         if(result)  {
             let token = jwt.sign({email: email, userid: user._id}, "shhh");
             res.cookie("token" , token);
-            res.status(200).send("you can login");
+            res.status(200).redirect("/profile");
             
         }
         else        
-            res.redirect("/login")
+            res.redirect("/login" , {user});
     });
 });
+
+app.get("/profile", isLoggedIn, async (req ,res ) =>{
+    let user = await userModel.findOne({email: req.user.email}).populate("posts");
+    res.render("profile" , {user});
+})
+
+app.post("/post", isLoggedIn, async (req ,res ) =>{
+    let user = await userModel.findOne({email: req.user.email});
+    let {content} = req.body;
+    let post = await postModel.create({
+        user: user._id,
+        content,
+    })
+
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile");
+})
+
+app.get("/like/:id", isLoggedIn, async (req ,res ) =>{
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    // console.log(req.user);
+    if(post.likes.indexOf(req.user.userid) === -1){
+        post.likes.push(req.user.userid);
+    }else{
+        post.likes.splice(post.likes.indexOf(req.user.userid) , 1);
+    }
+    await post.save()
+    res.redirect("/profile" );
+})
+
+app.get("/edit/:id", isLoggedIn, async (req ,res ) =>{
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    
+    res.render("edit" ,{post});
+})
+
+app.post("/update/:id", isLoggedIn, async (req ,res ) =>{
+    let post = await postModel.findOneAndUpdate({_id: req.params.id} , {content: req.body.content});
+    res.redirect("/profile");
+})
+
+app.get("/delete/:id", isLoggedIn, async (req ,res ) =>{
+    let post = await postModel.findOneAndDelete({_id: req.params.id});
+    res.redirect("/profile");
+})
+
 
 app.get("/logout" , (req , res) =>{
     res.cookie("token" , "");
@@ -77,13 +121,12 @@ app.get("/logout" , (req , res) =>{
 })
 
 function isLoggedIn(req , res , next){
-    if(req.cookies.token === "") res.send("You must be login");
+    if(req.cookies.token === "") res.redirect("/login");
     else{
         let data = jwt.verify(req.cookies.token, "shhh");
         req.user = data;
         next();
     }
-    next();
     
 }
 
